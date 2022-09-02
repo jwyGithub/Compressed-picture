aa8aaa112ea1bcdf5667844e723a87dc32c1aca4
jiangwy <jiangwy@staryea.com>
2022-09-02 14:03:47 +0800

    feature : update base path

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress';
 
 export default defineConfig({
     title: 'graph-lib',
-    base: '/docs/',
+    base: '/',
     themeConfig: {
         nav: [
             {
