24d09ccde88582bfc72b6a22ec6b885e9ce8bae4
jiangwy <jiangwy@staryea.com>
2022-09-02 13:47:16 +0800

    feature : edit md

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -18,38 +18,38 @@ export default defineConfig({
                 text: 'Guide',
                 items: [
                     { text: 'introduction', link: '/' },
-                    { text: 'Getting Started', link: '/guide/getting-started.md' }
+                    { text: 'Getting Started', link: '/guide/getting-started' }
                 ]
             },
             {
                 text: 'Config',
-                items: [{ text: 'Global Config', link: '/config/global-config.md' }]
+                items: [{ text: 'Global Config', link: '/config/global-config' }]
             },
             {
                 text: 'Draw',
                 items: [
-                    { text: 'Draw', link: '/draw/draw.md' },
-                    { text: 'Draw Anchor', link: '/draw/draw-anchor.md' }
+                    { text: 'Draw', link: '/draw/draw' },
+                    { text: 'Draw Anchor', link: '/draw/draw-anchor' }
                 ]
             },
             {
                 text: 'Style',
                 items: [
-                    { text: 'Global Style', link: '/style/register-global-style.md' },
-                    { text: 'Default Style', link: '/style/default-style.md' }
+                    { text: 'Global Style', link: '/style/register-global-style' },
+                    { text: 'Default Style', link: '/style/default-style' }
                 ]
             },
             {
                 text: 'Shape',
-                items: [{ text: 'Global Shape', link: '/shape/global-shape.md' }]
+                items: [{ text: 'Global Shape', link: '/shape/global-shape' }]
             },
             {
                 text: 'Cell',
                 items: [
                     { text: '初始化', link: '/cell/init.md' },
-                    { text: '获取所有Vertices', link: '/cell/getVertices.md' },
-                    { text: '获取所有Edges', link: '/cell/getEdges.md' },
-                    { text: '设置cell样式', link: '/cell/setCellStyle.md' }
+                    { text: '获取所有Vertices', link: '/cell/getVertices' },
+                    { text: '获取所有Edges', link: '/cell/getEdges' },
+                    { text: '设置cell样式', link: '/cell/setCellStyle' }
                 ]
             }
         ],
